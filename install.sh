e00ef404c3b309285fbc7937829fe9ed3fd2790a
RafatharUserbotV4 <ayangamaliaa@gmail.com>
2025-08-29 18:09:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "alfixkiabdg" ]; then
+  if [ "$USER_TOKEN" = "alfibandung" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Gih Ke RafatharCode X Amalia${NC}"
